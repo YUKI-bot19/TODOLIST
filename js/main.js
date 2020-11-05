304f53be2b116a101db97f2c56e7d1d0a5626b90
YUKI-bot19 <design.kcac@gmail.com>
2020-11-05 16:03:05 +0900

    main.jsの修正を再度行いました。

--- js/main.js
+++ js/main.js
@@ -1,59 +1,39 @@
-
 'use strict'
-
 {
-
   const addButton = document.getElementById('add');
   const taskValue = document.getElementById('text');
   const table = document.getElementById('table');
   const tbody = document.createElement('tbody');
-
   table.appendChild(tbody);
-
-  const items = [];
-
-  const showTasks = items => {
-
+  const todos = [];
+  const showTasks = todos => {
     tbody.textContent = '';
-
-    items.forEach((item, i) => {
+    todos.forEach((todo, idx) => {
       const row = tbody.insertRow(-1);
       tbody.appendChild(row);
-
       const tableId = row.insertCell(-1);
       const tableComment = row.insertCell(-1);
       const tableAction = row.insertCell(-1);
-
-      tableId.textContent = i;
-      tableComment.textContent = item.comment;
-
+      tableId.textContent = idx;
+      tableComment.textContent = todo.comment;
       row.appendChild(tableId);
       row.appendChild(tableComment);
       row.appendChild(tableAction);
-
       const workingButton = document.createElement('button');
       const removeButton = document.createElement('button');
-
       workingButton.textContent = '作業中';
       removeButton.textContent = '削除';
-
       tableAction.appendChild(workingButton);
       tableAction.appendChild(removeButton);
-
     });
   }
-
   addButton.addEventListener('click', () => {
-
-    items.push({
-      id: items,
+    const todo = ({
+      id: todos.length,
       comment: taskValue.value,
     });
-
-    console.log(items);
+    todos.push(todo);
     taskValue.value = '';
-    showTasks(items);
-
+    showTasks(todos);
   });
-
 }
