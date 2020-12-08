6ce3b984846423be6f1cbde9caee0ed40bd45e37
YUKI-bot19 <design.kcac@gmail.com>
2020-12-09 03:08:16 +0900

    ご確認の程よろしくお願い致します

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
   const taskValue = document.getElementById('text');
   const table = document.getElementById('table');
   const tbody = document.createElement('tbody');
+  const radioButtonAll = document.getElementById('radio-all-select');
+  const radioButtonWorking = document.getElementById('radio-working-select');
+  const radioButtonDone = document.getElementById('radio-done-select');
+  const radioButton = document.getElementsByName('radio-name');
   table.appendChild(tbody);
   const todos = [];
   const showTasks = todos => {
@@ -19,12 +23,39 @@
       row.appendChild(tableId);
       row.appendChild(tableComment);
       row.appendChild(tableAction);
-      const workingButton = document.createElement('button');
-      workingButton.textContent = '作業中';
-      tableAction.appendChild(workingButton);
+      tableAction.appendChild(createStatusButton(todo));
       tableAction.appendChild(createDeleteButton(row, todo.id));
     });
   }
+  const createStatusButton = (todo) => {
+    const statusButton = document.createElement('button');
+    statusButton.textContent = todo.status;
+    statusButton.addEventListener('click', () => {
+      if (todo.status === '作業中') {
+        todo.status = '完了';
+        filterTodos()
+      } else {
+        todo.status = '作業中';
+        filterTodos();
+      }
+    });
+    return statusButton;
+  };
+  const filterTodos = () => {
+    if (radioButtonAll.checked) {
+      return showTasks(todos);
+    } else if (radioButtonWorking.checked) {
+      const doingTodos = todos.filter(todo => {
+        return todo.status === '作業中'
+      });
+      return showTasks(doingTodos);
+    } else if (radioButtonDone.checked) {
+      const doneTodos = todos.filter(todo => {
+        return todo.status === '完了'
+      });
+      return showTasks(doneTodos);
+    }
+  };
   const createDeleteButton = (row, id) => {
     const deleteButton = document.createElement('button');
     deleteButton.textContent = '削除';
@@ -41,10 +72,16 @@
     });
     return deleteButton;
   };
+  radioButton.forEach((status, number) => {
+    radioButton[number].addEventListener('click', () => {
+      filterTodos();
+    });
+  });
   addButton.addEventListener('click', () => {
     const todo = ({
       id: todos.length,
       comment: taskValue.value,
+      status: '作業中'
     });
     todos.push(todo);
     taskValue.value = '';
